294cadd564bcd42399eb5134c9d3838d6f7e4f1d
Sheeper152 <winiarczyk.bw@gmail.com>
2024-01-18 21:16:12 +0100

    .

--- Project1/src/main/java/pl/coderslab/Main.java
+++ Project1/src/main/java/pl/coderslab/Main.java
@@ -1,6 +1,7 @@
 package pl.coderslab;
 
 import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.xml.sax.helpers.ParserAdapter;
 
 import javax.swing.plaf.synth.SynthOptionPaneUI;
@@ -16,9 +17,9 @@ import java.util.ListIterator;
 import java.util.Scanner;
 
 public class Main {
-    static final String FILE_NAME = "tasks.csv";
+    static final String FILE_NAME = "/home/bartosz/Dokumenty/kurs/Workshop1/Project1/tasks.csv";
     static final String[] OPTIONS = {"add", "remove", "list", "exit"};
-
+    static String[][] tasks;
 
     public static void main(String[] args) {
         System.out.println();
@@ -27,14 +28,12 @@ public class Main {
         for (int i = 0; i < OPTIONS.length; i++) {
             System.out.println(ConsoleColors.RESET + OPTIONS[i]);
         }
-        tasks = PobieranieDanych(FILE_NAME);
-        tasks = DodawanieDanych(FILE_NAME);
-        tasks = UsuwanieDanych(FILE_NAME);
+        tasks = pobieranieDanych(FILE_NAME);
+        tasks = wybieranieOpcji(FILE_NAME);
+
     }
 
-    static String[][] tasks;
-
-    public static String[][] PobieranieDanych(String FileName) {
+    public static String[][] pobieranieDanych(String FileName) {
 
         Path dir = Paths.get(FILE_NAME);
         if (!Files.exists(dir)) {
@@ -62,34 +61,42 @@ public class Main {
         return tab;
     }
 
-    public static String[][] UsuwanieDanych(String NazwaPliku) {
-        Path path1 = Paths.get("/home/bartosz/Dokumenty/kurs/Workshop1/Project1/tasks.csv");
-        Scanner scanner = new Scanner(System.in);
+    public static boolean czyNumerWiększyRównyZero(String input) {
 
-        String tab[][] = tasks;
-        int index = 0;
-        try {
-            for (int i = 0; i < tab.length; i++) {
-                System.out.println("Wpisz wiersz do usunięcia.");
-                index = scanner.nextInt();
-                if (index < tab.length) {
-                    tasks = ArrayUtils.remove(tab, index);
+        if (NumberUtils.isParsable(input)) {
+            return Integer.parseInt(input) >= 0;
 
-                }
-
-
-            }
-
-            main(OPTIONS);
-
-
-        } catch (ArrayIndexOutOfBoundsException ex) {
-            System.out.println("Nie ma takiego elementu w tabeli.");
         }
-        return tasks;
+        return false;
     }
 
-    public static String[][] DodawanieDanych(String FileName2) {
+    public static int wpiszNumer() {
+        Path path1 = Paths.get("/home/bartosz/Dokumenty/kurs/Workshop1/Project1/tasks.csv");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Wpisz numer do usunięcia");
+
+        String n = scanner.nextLine();
+        while (!czyNumerWiększyRównyZero(n)) {
+            System.out.println("Niepoprawna dana. Wpisz numer większy lub równy 0");
+            n = scanner.nextLine();
+
+        }
+        return Integer.parseInt(n);
+    }
+
+    private static void usuwanieWiersza(String[][] tab, int index) {
+
+        try {
+            if (index < tab.length) {
+                tasks = ArrayUtils.remove(tab, index);
+            }
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            System.out.println("Element not exist in tab");
+        }
+
+    }
+
+    public static String[][] wybieranieOpcji(String FileName2) {
 
         Path path1 = Paths.get("/home/bartosz/Dokumenty/kurs/Workshop1/Project1/tasks.csv");
         Scanner scanner = new Scanner(System.in);
@@ -97,33 +104,44 @@ public class Main {
 
         String dodajZmienna = scanner.nextLine();
 
+        try {String tab[][] = tasks;
 
-        try {
-            String tab[][] = tasks;
+
             if (dodajZmienna.equals("add")) {
 
                 for (int i = 0; i < tab.length; i++) {
                     System.out.println("Please add task description");
                     String dodanieZdania = "\n" + scanner.nextLine() + ", ";
                     Files.writeString(path1, dodanieZdania, StandardOpenOption.APPEND);
+
                     for (int j = 0; j < tab[i].length; j++) ;
                     System.out.println("Podaj datę");
                     String dodanieDaty = scanner.nextLine() + ", ";
                     Files.writeString(path1, dodanieDaty, StandardOpenOption.APPEND);
-                    System.out.println("Podaj ważność true/false");
-                    String isItImportant = scanner.nextLine();
-                    Files.writeString(path1, isItImportant, StandardOpenOption.APPEND);
-
-                    main(OPTIONS);
-
+                    System.out.println("Wpisz true/false");
+                    String czyJestWazne = scanner.nextLine();
+                    if (czyJestWazne.equals("true") && czyJestWazne.equals("false")) {
+                        Files.writeString(path1, czyJestWazne, StandardOpenOption.APPEND);
+                        main(OPTIONS);
+                    } else {
+                        System.out.println("Wpisz true/fase");
+                        czyJestWazne = scanner.nextLine();
+                    }
                 }
-            } else if (dodajZmienna.equals("exit")) {
 
-                System.out.println("bye bye");
+            } else if (dodajZmienna.equals("exit")) {
+                zapiszDoPliku(FILE_NAME, tasks);
+                System.out.println(ConsoleColors.RED + "Bye, bye.");
                 System.exit(0);
 
+
             } else if (dodajZmienna.equals("remove")) {
-                tasks = UsuwanieDanych(FILE_NAME);
+
+                usuwanieWiersza(tasks, wpiszNumer());
+                System.out.println("Wartość została usunięta");
+                zapiszDoPliku(FILE_NAME, tasks);
+                main(OPTIONS);
+
 
             } else if (dodajZmienna.equals("list")) {
 
@@ -132,22 +150,37 @@ public class Main {
                     for (int j = 0; j < tasks[i].length; j++) {
                         System.out.print(Arrays.asList(tasks[i][j]));
                     }
-                }
-                main(OPTIONS);
+                }main(OPTIONS);
+
+
             } else {
                 System.out.println("Please select correct option");
                 main(OPTIONS);
-
             }
+
         } catch (
                 IOException exeption) {
             System.out.println("wpisuj jak nalezy");
 
         }
-
-
         return tasks;
     }
+
+    public static void zapiszDoPliku(String fileName, String[][] tab) {
+        Path path1 = Paths.get("/home/bartosz/Dokumenty/kurs/Workshop1/Project1/tasks.csv");
+        String[] lines = new String[tasks.length];
+        for (int i = 0; i < tab.length; i++) {
+
+            lines[i] = String.join(",", tab[i]);
+        }
+
+        try {
+            Files.write(path1, Arrays.asList(lines));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
+
 
 
